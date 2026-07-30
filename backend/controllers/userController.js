d57d7bb037d66b7eb21d1a6814f048ab97fbdf0e
Subrat Panda <subratpanda018@gmail.com>
2026-07-30 23:13:13 +0530

    Implement user registration and login functionality with validation and authentication.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,12 @@
+import validator from 'validator'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import userModel from '../models/userModel.js'
 
+
+const createToken = (id) => {
+    return jwt.sign({id},process.env.JWT_SECRET)
+}
 //Route for user login
 const loginUser = async (req,res) => {
 
@@ -6,7 +14,46 @@ const loginUser = async (req,res) => {
 
 //Route for user registration
 const registerUser = async (req,res) => {
-    res.json({msg:"Register API working"})
+    
+    try {
+
+        const {name, email, password} = req.body
+
+        // checking user exist or not
+        const exists = await userModel.findOne({email})
+        if (exists) {
+            return res.json({success:false, message: "user already exists"})
+        }
+
+        //validating email format and strong password
+        if (!validator.isEmail(email)) {
+            return res.json({success:false, message: "please enter a valid email"})
+        }
+
+        if (password.length < 8) {
+            return res.json({success:false, message: "please enter a strong password"})
+        }
+
+        // Hashing user password
+        const salt = await bcrypt.genSalt(10)
+        const hasheedPassword = await bcrypt.hash(password,salt)
+
+        const newUser = new userModel({
+            name,
+            email,
+            password:hasheedPassword
+        })
+
+        const user = await newUser.save()
+
+        const token = createToken(user._id)
+
+        res.json({success:true,token})
+
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:error.message})
+    }
 }
 
 //Route for admin login
